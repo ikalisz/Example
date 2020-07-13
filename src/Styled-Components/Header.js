70e4f7076520ad90a8e7aa24f3b94a07dfd7ab8b
Ian Kaliszewski <ikalisz@yahoo.com>
2020-07-13 14:57:05 -0600

    initial commit

---
+++ src/Styled-Components/Header.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const HeaderExample = styled.div`
+  background-color: blue;
+  height: 100px;
+`
+
+const SubHeader = styled.div`
+  background-color: green;
+  height: 100px;
+`
+
+export default {
+  HeaderExample,
+  SubHeader,
+}
---
+++ src/Styled-Components/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Header from './Header'
+
+
+
+function Example() {
+  const {
+    HeaderExample,
+  } = Header
+
+  return (
+    <div>
+      <HeaderExample>
+      </HeaderExample>
+      <Header.SubHeader>
+      </Header.SubHeader>
+    </div>
+  )
+}
+
+export default Example
